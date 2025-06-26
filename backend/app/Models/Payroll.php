e66e0542505f6f62dbb5beb0e97fed7542e99b3e
Abdelrhmannali <abdelrhmanali981@gmail.com>
2025-06-26 21:36:31 +0300

    payroll with summary and recalulate functions  backend

--- backend/app/Models/Payroll.php
+++ backend/app/Models/Payroll.php
@@ -8,4 +8,26 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Payroll extends Model
 {
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'employee_id',
+        'month',
+        'month_days',
+        'attended_days',
+        'absent_days',
+        'total_overtime',
+        'total_deduction',
+        'total_deduction_amount',
+        'late_deduction_amount',
+        'absence_deduction_amount',
+        'total_bonus_amount',
+        'net_salary',
+        'salary_per_hour', 
+    ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class);
+    }
 }
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -16,12 +16,16 @@ use App\Http\Controllers\Auth\PasswordResetController;
 Route::post('/hr/forgot-password', [PasswordResetController::class, 'sendResetLink']);
 Route::post('/hr/reset-password', [PasswordResetController::class, 'reset']);
 
-
+// ==================
+// Public routes
+// ==================
 
 Route::post('/hr/login', [HRController::class, 'login']);
         Route::post('/hr/AddHr', [HRController::class, 'AddHr']);
 
-
+// ==================
+// Protected routes
+// ==================
 Route::middleware('auth:sanctum')->group(function () {
 
    
@@ -33,6 +37,19 @@ Route::middleware('auth:sanctum')->group(function () {
 
     Route::post('/hr/logout', [HRController::class, 'logout']);
 
-    
+
+
+
+    // Payroll
+    Route::prefix('payroll')->group(function () {
+        Route::get('/show', [PayrollController::class, 'show']);
+        Route::get('/summary', [PayrollController::class, 'summary']);
+        Route::get('/all-employees-data', [PayrollController::class, 'allEmployeesData']);
+        Route::post('/recalculate', [PayrollController::class, 'recalculate']);
+        Route::get('/all', [PayrollController::class, 'allPayrolls']); 
+Route::get('/current-month', [PayrollController::class, 'getCurrentMonth']);
+Route::get('/all-months', [PayrollController::class, 'getAllMonths']);
+
+    });
 
 });
